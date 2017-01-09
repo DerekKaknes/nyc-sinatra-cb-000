c43edbafe8f9b1ca4126658fb233ad3cce43e7fa
Derek Kaknes <derek.kaknes@gmail.com>
2017-01-09 15:41:48 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,4 +13,18 @@ class FiguresController < ApplicationController
     @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
+
+  post '/figures' do
+    binding.pry
+    figure = Figure.create(params[:figure])
+    params[:figure][:title_ids].each do |tid|
+      figure.titles << Title.find(tid)
+    end
+    if !params[:title].empty?
+      figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark].empty?
+      figure.landmarks << landmark.create(params[:landmark])
+    end
+  end
 end
